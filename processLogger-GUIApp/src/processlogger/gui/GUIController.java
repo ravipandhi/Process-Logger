75a0b8f4900bf9d216c2c834518c56aff8380e03
Ravi Pandhi <ravipandhiait@gmail.com>
2016-12-31 14:26:32 +0530

    for sending commands to the GUI as per the interaction with applicationController

---
+++ processLogger-GUIApp/src/processlogger/gui/GUIController.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package processlogger.gui;
+
+/**
+ *
+ * @author maharshi
+ */
+public class GUIController {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        log_gen lg = new log_gen();
+        lg.setVisible(true);
+    }
+}
